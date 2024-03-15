90718393cb59e02d3cd661b7d309717b249e21e9
cmurillo <christopherrmg@gmail.com>
2024-03-14 20:59:22 -0600

    condicionales

---
+++ condiciones.py
@@ -0,0 +1,25 @@
+# DE LAS ESTRUCTURAS MAS IMPORTANTES EN PYTHON SON LAS CONDICIONALES
+# IF
+if False:
+    print("La condición es verdadera")  
+
+edad = 18
+teorico = True
+
+if edad >= 18 and teorico:
+    print("Puedes sacar licecnia de conducir")
+else:
+    print("No puedes sacar licencia de conducir")
+
+# IF-ELIF-ELSE
+
+nota = 98
+
+if nota >= 70 and nota <= 100:
+    print("Aprobado")
+elif nota >= 60 and nota < 70:    
+    print("A recuperatorio")
+elif nota >= 0 and nota < 60:
+    print("Desaprobado")
+else:
+    print("Nota incorrecta no sea caballo")
--- hola.py
+++ hola.py
@@ -1,1 +1,62 @@
-print("HOLA MUNDO")
+# VAMOS A HABLAR SOBRE LOS TIPOS DE VARIABLES EN PYTHON
+# Variables
+# Enteros
+a = int(input("Ingrese un número entero: "))
+b = int(input("Ingrese otro número entero: "))
+c = 15
+# Flotantes
+d = 5.5
+e = 10.5
+f = 15.5
+# Cadenas
+g = "Hola"
+h = "Mundo"
+i = "Python"
+# Booleanos
+j = True
+k = False
+
+# Imprimir las variables
+print("El valor de la variable a y b respectivamente son:",a,"y",b)
+
+# AHORA VAMOS A HABLAR SOBRE LAS OPERACIONES ARITMÉTICAS EN PYTHON
+# Suma 
+print("La suma de a y b es:",a+b)
+# TAMBIEN PUEDO ALMALCENAR DENTRO DE UNA VARIABLE EL RESULTADO DE UNA OPERACIÓN
+suma = a+b
+print("La suma de a y b es:",suma)
+
+# Resta
+print("La resta de a y b es:",a-b)
+
+# Multiplicación
+print("La multiplicación de a y b es:",a*b)
+
+# División
+print("La división de a y b es:",a/b)
+
+# Módulo
+print("El módulo de a y b es:",a%b)
+
+# Potencia
+print("La potencia de a y b es:",a**b)
+
+# TAMBIEN PODEMOS HACER COMPARTACIONES ENTRE VARIABLES
+# Mayor que
+print("a es mayor que b:",a>b)
+
+# Menor que
+print("a es menor que b:",a<b)
+
+# Mayor o igual que
+print("a es mayor o igual que b:",a>=b)
+
+# Menor o igual que
+print("a es menor o igual que b:",a<=b)
+
+# Igual que
+print("a es igual que b:",a==b)
+
+# Diferente que
+print("a es diferente que b:",a!=b)
+
